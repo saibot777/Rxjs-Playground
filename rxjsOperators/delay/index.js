98238e2373a3b6d6e6536725618f1674f90e1bc9
saibot777 <stefantrajkovic777@gmail.com>
2018-04-18 15:36:55 +0200

    delay operator implemented

---
+++ rxjsOperators/delay/index.js
@@ -0,0 +1,13 @@
+import Rx from 'rxjs';
+
+Rx.Observable.of(42)
+    .do(() => console.log("What is the this?"))
+    .delay(1200)
+    .do(() => console.log("TELL MEE!!!"))
+    .delay(2000)
+    .subscribe(x => console.log(x));
+
+console.log("STARTING SECOND SEQUENCE")
+Rx.Observable.range(1, 9)
+    .delayWhen(x => Rx.Observable.timer(x * 1000))
+    .subscribe(x => console.log(x));
